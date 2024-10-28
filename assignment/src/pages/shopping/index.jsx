5c48dd166f918dc482127b8aa58ddf697b29eac9
huyleo03 <giahuyn495@gmail.com>
2024-10-28 10:34:30 +0700

    edit

--- assignment/src/pages/shopping/index.jsx
+++ assignment/src/pages/shopping/index.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import Breadcrumb from "../../component/shopping/Breadcrumb";
 import FilterPrice from "../../component/shopping/FilterPrice";
 import FilterColor from "../../component/shopping/FilterColor";
 import FilterSize from "../../component/shopping/FilterSize";
 import ShopProduct from "./ShopProduct";
+
 const Shopping = () => {
+  const [priceFilter, setPriceFilter] = useState([]);
   return (
     <React.Fragment>
       <Breadcrumb />
       <div className="container-fluid">
         <div className="row px-xl-5">
           <div className="col-lg-3 col-md-4">
-            <FilterPrice />
+            <FilterPrice onFilterChange={setPriceFilter} />
             <FilterColor />
             <FilterSize />
           </div>
-          <ShopProduct />
+          <ShopProduct priceFilter={priceFilter} />
         </div>
       </div>
     </React.Fragment>
